2f8f1bebaa1b54027d1e9e4e98fccc17d78741c0
Alex Berke <aberke13@us155648.local>
2013-10-22 10:06:21 -0400

    added java.jdbc for sql

--- project.clj
+++ project.clj
@@ -9,7 +9,8 @@
                  [ring/ring-devel "1.1.0"]
                  [ring-basic-authentication "1.0.1"]
                  [environ "0.2.1"]
-                 [com.cemerick/drawbridge "0.0.6"]]
+                 [com.cemerick/drawbridge "0.0.6"]
+                 [org.clojure/java.jdbc "0.3.0-alpha5"]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.2.1"]]
   :hooks [environ.leiningen.hooks]
