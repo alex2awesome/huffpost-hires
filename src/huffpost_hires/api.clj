d59ff06039d9da0df4d1fe790d6be6917541c367
Alex Berke <aberke13@us155648.local>
2013-10-23 14:54:04 -0400

    ugly if statements now case statements

--- src/huffpost_hires/api.clj
+++ src/huffpost_hires/api.clj
@@ -46,47 +46,47 @@
 	"Returns all completed tasks for applicant"
 	[request]
 	(println (str "/api/applicant/incomplete-tasks with request:" request))
-	(models/query-json [(str "select * from tasks where applicant=" (request :id))]))
+	(models/query-json [(str "select * from tasks WHERE applicant=" (request :id) " AND completed=FALSE")]))
 
 ;; /api/interviewer/complete-tasks?id='interviewID'
 (defn complete-tasks-by-interviewer
 	"Returns all completed tasks for interviewer"
 	[request]
 	(println (str "/api/interviewer/complete-tasks with request:" request))
-	(models/query-json [(str "select * from tasks where interviewer=" (request :id))]))
+	(models/query-json [(str "select * from tasks where interviewer=" (request :id) " AND completed=TRUE")]))
 
 ;; /api/interviewer/incomplete-tasks?id='interviewID'
 (defn incomplete-tasks-by-interviewer
 	"Returns all completed tasks for interviewer"
-	[])
+	[]
+	(models/query-json [(str "select * from tasks where interviewer=" (request :id) " AND completed=FALSE")]))
 
+;; routing helper fomr handle-get-request
 (defn handle-get-request-applicant
 	"Called upon GET request to url /api/applicant/*"
 	[route params] ; route == * in the GET request
 	(println (str "api/handle-get-request-applicant with route: " route "; params: " params))
-	(if (= route "")
-		(applicant params)
-		(if (= route "all")
-			(applicants-all)
-			(if (= route "complete-tasks")
-				(complete-tasks-by-applicant params)
-				(if (= route "incomplete-tasks")
-					(incomplete-tasks-by-applicant params)
-					(str "Invalid api request to /api/applicant/" route))))))
+	(case route
+		"" (applicant params)
+		"all" (applicants-all)
+		"complete-tasks" (complete-tasks-by-applicant params)
+		"incomplete-tasks" (incomplete-tasks-by-applicant params)
+		(str "Invalid api request to /api/applicant/" route)))
 
+
+;; routing helper fomr handle-get-request
 (defn handle-get-request-interviewer
 	"Called upon GET request to url /api/interviewer/*"
 	[route params] ; route == * in the GET request
 	(println (str "api/handle-get-request-interviewer with route: " route "; params: " params))
-	(if (= route "")
-		(interviewer params)
-		(if (= route "all")
-			(interviewers-all)
-			(if (= route "complete-tasks")
-				(complete-tasks-by-interviewer params)
-				(if (= route "incomplete-tasks")
-					(incomplete-tasks-by-interviewer params)
-					(str "Invalid api request to /api/interviewer/" route))))))
+
+	(case route
+		"" (interviewer params)
+		"all" (interviewers-all)
+		"complete-tasks" (complete-tasks-by-interviewer params)
+		"incomplete-tasks" (incomplete-tasks-by-interviewer params)
+		(str "Invalid api request to /api/interviewer/" route)))
+
 
 (defn handle-get-request
 	"Called by web upon GET request to url /api/*/*"
@@ -96,7 +96,7 @@
 	(let [params (request :params) route (params :*) route-prefix (first route) route-suffix (second route)]
 		(println (str "params: " params))
 		(println (str "route:" route))
-		(case (first route)
+		(case route-prefix
 			"applicant" (handle-get-request-applicant route-suffix params)
 			"interviewer" (handle-get-request-interviewer route-suffix params)
 			"Invalid api request")))
